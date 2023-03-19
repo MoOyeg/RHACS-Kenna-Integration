49802e7c3be81d55219f6750340b971a6eb50d5f
MoOyeg <mojojoye@yahoo.com>
2023-03-18 22:45:40 -0400

    Creating First Draft for ACS-Kenna-Prototype

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RHACS-Kenna-Integration
-Red Hat ACS Integration for Vulnerability Information with KENNA
+Red Hat ACS Integration for exporting vulnerability Information with KENNAfrom RHACS into Kenna KDE Format.
+
+## Status: Prototype
+
+## How to run
+- pip install -r ./app/requirements.txt
+- python ./run.py
+- Point [ACS WebHook](https://docs.openshift.com/acs/3.74/integration/integrate-using-generic-webhooks.html) to use Configured Ip/Port in [run.py](./run.py)
+
+## How to run test
+- pytest
--- app/main.py
+++ app/main.py
@@ -8,12 +8,82 @@ from concurrent.futures import ThreadPoolExecutor
 from logging import getLogger, config
 from os import getenv
 from typing import Optional  # pylint: disable=import-error
-from pydantic import BaseModel  # pylint: disable=import-error
-from fastapi import FastAPI,Request,Body, Response # pylint: disable=import-error
+from pydantic import BaseModel,Extra  # pylint: disable=import-error
+from fastapi import FastAPI,Request,Body,Response # pylint: disable=import-error
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
+from fastapi.encoders import jsonable_encoder
 from os import path
 import json
+
+class KDEVulnDef(BaseModel):
+    scanner_type: str
+    cve_identifiers: str | None
+    wasc_identifiers: str | None
+    cwe_identifiers: str | None
+    name: str
+    description: str
+    solution: str
+
+class KDEFinding(BaseModel):
+    scanner_identifier: str
+    scanner_type: str
+    created_at: str | None
+    due_date: str | None
+    last_seen_at: str
+    severity: int | None
+    triage_state: str	 	
+    additional_fields: list | None
+    vuln_def_name: str
+
+class KDEvuln(BaseModel):
+    scanner_identifier: str
+    scanner_type: str 
+    scanner_score: int
+    override_score: int | None
+    created_at: str | None
+    last_seen_at: str | None
+    last_fixed_on: str | None
+    status: str | None
+    port: int 
+    vuln_def_name: str    
+
+class KDElocator_field(BaseModel):
+    file: str | None
+    ip_address: str | None
+    mac_address: str | None
+    hostname: str | None
+    ec2: str | None
+    netbios: str | None
+    url: str | None
+    fqdn: str | None
+    image_id: str | None
+    container_id: str | None
+    external_id: str | None	
+    database: str | None
+
+class KDEAsset(BaseModel,extra=Extra.allow):
+    # KDElocator_field: KDElocator_field
+    application: str | None
+    tags: list[str] | None
+    owner: str | None
+    os: str | None
+    os_version: str | None
+    priority: str | None
+    asset_type: str | None
+    vulns: str | None
+    findings: str | None
+      
+class KDEJsonv2(BaseModel):
+    skip_autoclose: bool = False
+    version: int = 2
+    assets: list[KDEAsset]
+    reset_tags: bool = False
+    #Commenting to allow parsing at the moment,needs work.
+    # vulns: list[KDEvuln]
+    # findings:  list[KDEFinding]
+    
+   
 
 class ACSViolations(BaseModel):
     message: str
@@ -24,7 +94,7 @@ class ACSImageDetails(BaseModel):
     fullName: str
     
 class ACSImage(BaseModel):
-    id: str
+    id: str | None = None
     name: ACSImageDetails
 
 class ACSContainer(BaseModel):
@@ -71,28 +141,38 @@ class ACSAlert(BaseModel):  # pylint: disable=too-few-public-methods
     time: str
     firstOccurred: str
 
-       
-json_object = []
-count = 0
-# setup loggers
-
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
 config.fileConfig(log_file_path, disable_existing_loggers=False)
 logger = getLogger("logger_root")
-
+count=0
 # Declare App as a FastApi Object
 app = FastAPI()
 
 # Instance Hostname is global
 instance_hostname = ""
 
-# TO-DO Streamline determine functions into 1(Probably a custom decorator)
-
+#Write Output To KDE
+async def write_out_kde(acsalert:ACSAlert):
+    """ Write Received ACS Alert to KDE File"""
+    
+    #Create Deployment As KDE Assets
+    new_kdeasset=KDEAsset(external_id=acsalert.deployment.id)
+    if acsalert.deployment.labels is not None:
+        if new_kdeasset.tags is None:
+            new_kdeasset.tags = []
+        for key in acsalert.deployment.labels:
+            new_kdeasset.tags.append("{}:{}".format(key,acsalert.deployment.labels[key]))
+    new_kdejson=KDEJsonv2(assets=[new_kdeasset])
+    
+    f = open('./kde_output/{}.json'.format(acsalert.deployment.id),"a")  
+    f.write(json.dumps(jsonable_encoder(new_kdejson),indent=2))
+    f.close()
+    
 # Get Startup Information
 @app.on_event("startup")
 async def startup_event():
     '''Startup Function'''
-    logger.info("Starting up Integration Service")
+    logger.info("Starting up ACS/Kenna Integration Service")
     global instance_hostname  # pylint: disable=global-statement
     instance_hostname = getenv('HOSTNAME')
 
@@ -103,8 +183,13 @@ async def read_root():
     logger.info("Root Url '/' was Called")
     return {"Application": "Integration Service for Red Hat Advanced Cluster Security Service"}
 
+@app.get("/health")
+async def read_root():
+    '''Application Health URL'''
+    logger.info("Health Url was Called")
+    return {"status": "OK"}
 
-@app.post("/recieve_acs_vuln_alert")
+@app.post("/recieve_acs_vuln_alert_load")
 async def determine_metadata(request: Request):
     global count
     json_temp=await request.json()
@@ -118,14 +203,9 @@ async def determine_metadata(request: Request):
     count+=1
     return {"status": "OK"}
 
-@app.post("/recieve_acs_vuln_alert_test")
+@app.post("/recieve_acs_vuln_alert")
 async def determine_metadata(response: Response,request: Request,alert: ACSAlert=Body(embed=True)):
-    if response.status_code == 422:
-        json_temp=await request.json()
-        json_formatted_str = json.dumps(json_temp, indent=2)
-        print(f"\n")
-        print(json_formatted_str)
-        print(f"\n")
+    await write_out_kde(alert)
     return {"status": "OK"}
 
 def register_exception(app: FastAPI):
@@ -139,29 +219,3 @@ def register_exception(app: FastAPI):
         print(content)
         return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
     
-# @app.post("/recieve_acs_vuln_alert")
-# async def determine_metadata(alert: ACSAlert = Body(embed=True)):
-#     '''determine cloud'''
-#     logger.info("/metadata path was called")
-#     # json_temp=await request.json()
-#     # global json_object
-#     # payload: dict = Body(...)
-#     # json_object.append(json_temp)
-#     print(alert)
-#     return {"status": "OK"}
-#     # if metadata.cloudname == "unknown":
-#     #     result = False
-#     #     if q is None:
-#     #         for determine_function in determine_cloud_functions_list:
-#     #             result = await determine_function()
-#     #             if result[0] is True:
-#     #                 response_data = result[1]()
-#     #                 metadata.cloudname = result[2]
-#     #                 metadata.hostname = response_data["hostname"]
-#     #                 metadata.region = response_data["region"]
-#     #                 metadata.instance_type = response_data["instanceType"]
-#     #                 metadata.availability_zone = response_data["availability_zone"]
-#     #                 break
-#     #         return JSONResponse(metadata.todict())
-#     # else:
-#     #     return JSONResponse(metadata.todict())
